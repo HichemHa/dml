edbfa62eca77da1be1b1627f0ade09df328ef0ff
HichemHa <hichemhanafi012@gmail.com>
2021-02-24 16:28:04 +0100

    first commit

---
+++ dml.sql
@@ -0,0 +1,29 @@
+INSERT INTO
+    Product
+VALUES
+    ("P01", "Samsung Galaxy S20", "Smartphone", 3299);
+
+INSERT INTO
+    Product
+VALUES
+    ("P02", "ASUS Notebook", "PC", 4599);
+
+INSERT INTO
+    Customer
+VALUES
+    ("C01", "ALI", 71321009);
+
+INSERT INTO
+    Customer
+VALUES
+    ("C02", "ASMA", 77345823);
+
+INSERT INTO
+    Orders
+VALUES
+    ("C01", "P02", NULL, 2, 9198);
+
+INSERT INTO
+    Orders
+VALUES
+    ("C02", "P01", "28/05/2020", 1, 3299);
